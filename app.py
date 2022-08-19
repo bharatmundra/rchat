d58055a391308c4b53597f3d1fba1b520f6ef486
Bharat Mundra <bharatmundra119@gmail.com>
2022-08-19 15:52:15 +0530

    basics of page

---
+++ app.py
@@ -0,0 +1,17 @@
+from crypt import methods
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+app.secret_key= 'replace later'
+
+
+
+@app.route("/",methods=['get','post'])
+def index():
+
+    return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
