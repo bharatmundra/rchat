ac199af80abbcffbdccd5dd3d30dac55e85ddda5
Bharat Mundra <bharatmundra119@gmail.com>
2022-08-20 17:23:35 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from crypt import methods
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+
+
+from wtform_field import *
 
 app=Flask(__name__)
 
@@ -9,8 +12,11 @@ app.secret_key= 'replace later'
 
 @app.route("/",methods=['get','post'])
 def index():
-
-    return render_template("index.html")
+    
+    reg_form = RegistartionForm()
+    if reg_form.validate_on_submit():
+        return "great success"
+    return render_template("index.html",form=reg_form)
 
 
 if __name__=="__main__":
